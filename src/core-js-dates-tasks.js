00439de4202cb23718a1705013be385278315180
kkotes <seminae99@gmail.com>
2025-03-24 21:25:21 +0300

    feat: solve 8 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -141,8 +141,15 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const { start, end } = period;
+  const startValue = Date.parse(start);
+  const endValue = Date.parse(end);
+  const dateValue = Date.parse(date);
+  if (dateValue >= startValue && dateValue <= endValue) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -156,8 +163,22 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dateObj = new Date(date);
+
+  const year = dateObj.getUTCFullYear();
+  const month = dateObj.getUTCMonth() + 1;
+  const day = dateObj.getUTCDate();
+
+  let hours = dateObj.getUTCHours();
+  const a = hours >= 12 ? 'PM' : 'AM';
+  hours %= 12;
+  hours = hours || 12;
+
+  const minutes = String(dateObj.getUTCMinutes()).padStart(2, 0);
+  const seconds = String(dateObj.getUTCSeconds()).padStart(2, 0);
+
+  return `${month}/${day}/${year}, ${hours}:${minutes}:${seconds} ${a}`;
 }
 
 /**
