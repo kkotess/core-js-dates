67e491cd692318ffc77dabe671ae00c6ed91faeb
kkotes <seminae99@gmail.com>
2025-03-24 22:58:01 +0300

    feat: solve 9 tasks

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -193,8 +193,17 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const dateInMonth = new Date(year, month, 0).getDate();
+  let weekendCount = 0;
+  for (let day = 1; day <= dateInMonth; day += 1) {
+    const current = new Date(year, month - 1, day);
+    const dayOfWeek = current.getDay();
+    if (dayOfWeek === 6 || dayOfWeek === 0) {
+      weekendCount += 1;
+    }
+  }
+  return weekendCount;
 }
 
 /**
